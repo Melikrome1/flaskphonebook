81fb21417aba588746cf64bd587ff303d9142110
Cynthia Aguilar <melilarios@gmail.com>
2023-06-01 21:17:26 -0700

    address book db

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from app import db
+
+
+
+
+class Address(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+    phone_number = db.Column(db.String(20), nullable=False)
+    address = db.Column(db.String(200), nullable=True)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Address {self.first_name} {self.last_name}>'
