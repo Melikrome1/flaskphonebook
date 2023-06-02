e570fcbfca19bbbc37403f7d34f3492ee8443e8a
Cynthia Aguilar <melilarios@gmail.com>
2023-06-02 09:15:02 -0700

    new files

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-app = Flask(__name__, template_folder='templates')
-app.config['SECRET_KEY'] = 'your-secret-key'
 
-from app import routes
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///address_book.db'  
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+
+
+app.config['SECRET_KEY'] = 'CynCity'
+
+from app import routes, models
--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from wtforms.validators import Length
+from wtforms.validators import Optional
+
 
 class AddressForm(FlaskForm):
     first_name = StringField('First Name', validators=[DataRequired()])
@@ -8,3 +11,4 @@ class AddressForm(FlaskForm):
     phone_number = StringField('Phone number', validators=[DataRequired()])
     address = StringField('Address', validators=[DataRequired()])
     submit = SubmitField('Submit')
+    address = StringField('Address', validators=[Optional(), Length(max=200)])
